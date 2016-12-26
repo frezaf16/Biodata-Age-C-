3ba9e25acb8f55d1b9ed497d8190ebe2b90108e6
Faisal Rezza Fahlevi <frezaf99@gmail.com>
2016-12-26 21:15:55 +0700

    Create biodata.cpp

---
+++ biodata.cpp
@@ -0,0 +1,18 @@
+#include<conio.h>
+#include<iostream.h>
+main() {
+clrscr();
+char name[20], address[20];
+int year, age;
+cout<<"=============Program Bodata==============\n";
+cout<<"Input name    : "; cin>>name;
+cout<<"Input address  : "; cin>>address;
+cout<<"year of birth     : "; cin>>year;
+cout<<"=========================================\n\n";
+age=2016-year;
+cout<<"Your name is      : "<<nama<<"\n";
+cout<<"Your address is    : "<<alamat<<"\n";
+cout<<"Age You Are      : "<<umur<<"\n\n";
+cout<<"Press Enter To Return To Menu";
+getch();
+}
